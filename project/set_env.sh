d464e7ac411495242af3017c55ec8eb573b83669
upworksunil <sunilbabu.tvm@gmail.com>
2020-10-25 21:15:07 +0100

    Initial commit to develop

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
-export URL=http://localhost:8100
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=sunilbabu
+set POSTGRES_HOST=dbudagram.c8uua0plyoky.us-east-2.rds.amazonaws.com
+set POSTGRES_DB=dbudagram
+set AWS_BUCKET=udacity-415140894837-test
+set AWS_REGION=US East
+set AWS_PROFILE=AKIAJNS5SWBYBIDB6U4A
+set JWT_SECRET=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
+set URL=http://localhost:8100
